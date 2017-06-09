3ba441b4c9f5d672db6777b8ea68daed22663f78
Sourabh Singh <sourabhrishi25@gmail.com>
2017-06-09 13:43:35 +0530

    Added a zero static vector

--- PhysicsGame/GearVector.h
+++ PhysicsGame/GearVector.h
@@ -12,7 +12,8 @@ namespace Gear {
 		GearVector(float, float);
 		GearVector(const GearVector &);
 		~GearVector();
-
+		//Static
+		static GearVector ZERO;
 		//Utilities
 		float norm();										// Normalise the vector 
 		float len();										// Returns the length of the vector
